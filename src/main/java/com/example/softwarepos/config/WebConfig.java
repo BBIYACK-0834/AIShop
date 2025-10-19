827ee5fa979d07faef4bb65742216ea8e3678dd0
BBIYACK-0834 <80592530+BBIYACK-0834@users.noreply.github.com>
2025-10-19 13:58:10 +0000

    이미지 띄우는거까지 완료

--- src/main/java/com/example/softwarepos/config/WebConfig.java
+++ src/main/java/com/example/softwarepos/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.config;
+package com.example.softwarepos.config; // 본인 패키지명에 맞게 수정하세요
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,11 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       
         String uploadPath = "file:///workspaces/AIShop/uploads/";
-
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadPath);
     }
 }
-
